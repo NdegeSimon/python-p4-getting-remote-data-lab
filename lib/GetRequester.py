b485f7316911b27c16119ff1177524afb23c446e
Simon Ndege <simopetley@gmail.com>
2025-09-14 23:29:17 +0300

    second commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,14 +1,27 @@
+# import requests
+# import json
+
+# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+# class GetRequester:
+
+#     def __init__(self, url):
+#         self.url = url
+#         self.response = requests.get(url)
+#     def get_response_body(self):
+#         return self.response.text
+
+#     def load_json(self):
+#         return json.loads(self.response.text)
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
         self.response = requests.get(url)
+
     def get_response_body(self):
-        return self.response.text
+        return self.response.text.encode('utf-8')
 
     def load_json(self):
         return json.loads(self.response.text)
