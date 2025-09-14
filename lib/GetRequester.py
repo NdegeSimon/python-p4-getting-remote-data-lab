0f3bc20c51b82ca6ffe48089a2d6760bed6c261a
Simon Ndege <simopetley@gmail.com>
2025-09-14 23:18:44 +0300

    initial commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
-
+        self.response = requests.get(url)
     def get_response_body(self):
-        pass
+        return self.response.text
 
     def load_json(self):
-        pass
+        return json.loads(self.response.text)
